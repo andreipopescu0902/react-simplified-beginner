d28533f1b87851e0e4fbebbc306d7a5f2b538d18
Andrei Popescu <andrei.popescu@epg.ro>
2023-09-13 08:51:27 +0000

    useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
-import { useEffect } from "react"
-import { useState, useRef } from "react"
+import { useState } from "react"
 
-function App() {
-  const [name, setName] = useState("")
-  const alexRef = useRef("Alex")
-  console.log(alexRef.current)
+const LIST = Array(1000000)
+  .fill()
+  .map((_, i) => i+1)
 
-  useEffect(() => {
-    console.log("Re-render")
-  })
+  function App() {
+    const [query, setQuery] = useState("")
+    const [isDarkMode, setIsDarkMode] = useState(false)
 
-  return (
-    <>
-      <label>
-        Name:
-        <input value={name} onChange={e => setName(e.target.value)}/>
-      </label>
-    <button onClick={() => (alexRef.current = Math.random())}>Change Ref</button>
-    <button onClick={() => console.log(alexRef.current)}>Print Ref</button>
-    </>
-  )
-}
+    const filteredList = LIST.filter(n => n.toString().includes(query))
 
-export default App
+    console.log(filteredList.length)
+
+    return (
+      <div
+        style={{
+          background: isDarkMode ? "#333" : "white",
+          color: isDarkMode ? "white" : "#333",
+        }}>
+        <label>
+          Query: 
+          <input value={query} onChange={e => setQuery(e.target.value)}/>
+          </label>
+          <br />
+          <label>
+            <input
+              type="checkbox"
+              onChange={e => setIsDarkMode(e.target.checked)}
+              checked={isDarkMode}
+            />
+            Dark Mode
+          </label>
+        </div>
+
+    )
+  }
+
+  export default App
